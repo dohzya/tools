89344e0502b55a04aba29cdc4eca4d3e0e45a424
Etienne VALLETTE d'OSIA <code@dohzya.com>
2026-06-10 12:09:14 +0200

    chore(wl): finalize v0.18.1 release

--- homebrew/Formula/wl.rb
+++ homebrew/Formula/wl.rb
@@ -1,26 +1,26 @@
 class Wl < Formula
   desc "Worklog - track work progress during development sessions"
   homepage "https://github.com/dohzya/tools"
-  version "0.18.0"
+  version "0.18.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/dohzya/tools/releases/download/wl-v0.18.0/wl-darwin-arm64"
-      sha256 "32a3ceeada033ba0f199d3b60c3909f6ddf95326dd9216c6f1cf2ee26d6ec3ca"
+      url "https://github.com/dohzya/tools/releases/download/wl-v0.18.1/wl-darwin-arm64"
+      sha256 "6de04304d53f06f2de69299609aa156371bd5190c36c247f3f53eca4b7c874f1"
     elsif Hardware::CPU.intel?
-      url "https://github.com/dohzya/tools/releases/download/wl-v0.18.0/wl-darwin-x86_64"
-      sha256 "ca188e1517b5b4b702972d4628b45bb0c4f6855d9002b86895ad47d6f7f6e06e"
+      url "https://github.com/dohzya/tools/releases/download/wl-v0.18.1/wl-darwin-x86_64"
+      sha256 "2fcde2730bae517dab90f8f116d8e0a8f145eebefe258b31cf1d280cebb4d515"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/dohzya/tools/releases/download/wl-v0.18.0/wl-linux-arm64"
-      sha256 "c7b95d4170c67d0f1a8abd04b4cfe2ef3bcefacd0b59ef193b086d2ce3632ef8"
+      url "https://github.com/dohzya/tools/releases/download/wl-v0.18.1/wl-linux-arm64"
+      sha256 "58d34aac182113d41ffd539f187a34db69d58033448f61729b21bd931e78c0e9"
     elsif Hardware::CPU.intel?
-      url "https://github.com/dohzya/tools/releases/download/wl-v0.18.0/wl-linux-x86_64"
-      sha256 "93c0894066664b7f166ffbd90ff2395c33302a11f9b5c836bd0cb8497117ccd1"
+      url "https://github.com/dohzya/tools/releases/download/wl-v0.18.1/wl-linux-x86_64"
+      sha256 "a08e9b6508f6cd1eece1028bca299d8d3f88600df29952d143df300827f54588"
     end
   end
 
